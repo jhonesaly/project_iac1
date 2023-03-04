64d2bbb431fda4aa200e6301db4e76b44b753003
Jhones <jhonesaly@gmail.com>
2023-03-04 11:42:57 -0300

    Update structure of new_iac_advanced.sh

--- iac_advanced/new_iac_advanced.sh
+++ iac_advanced/new_iac_advanced.sh
@@ -1,52 +1,57 @@
 #!/bin/bash
 
+    GREEN='\033[0;32m'
+    NC='\033[0m'
+
 ## 0 - Saudação
-printf "\nIniciando protocolo new_iac...\n"
-printf "\nEm caso de dúvida, consulte a documentação disponível em <https://github.com/jhonesaly/project_iac1>\n"
+
+    printf "\n${GREEN}Iniciando protocolo new_iac...${NC}\n"
+    printf "\n${GREEN}Em caso de dúvida, consulte a documentação disponível em <https://github.com/jhonesaly/project_iac1>${NC}\n"
 
 ## 1 - Chama o script que exclui antigos diretórios, arquivos, grupos e usuários anteriores
 
-while true; do
+    while true; do
 
-    read -n 1 -p "Deseja apagar todos os diretórios, grupos e usuários anteriores? [y/n] " ans_b1
+        read -n 1 -p "Deseja apagar todos os diretórios, grupos e usuários anteriores? [y/n] " ans_b1
 
-    if [ $ans_b1 = "y" ]; then
-        ./modules/exclude_past_gud.sh
-        break
+        if [ $ans_b1 = "y" ]; then
+            ./modules/exclude_past_gud.sh
+            break
 
-    elif [ $ans_b1 = "n" ]; then
-        printf "\n...\n"
-        break
+        elif [ $ans_b1 = "n" ]; then
+            printf "\n...\n"
+            break
 
-    else
-        printf "\nDigite um comando válido.\n"
-        continue
+        else
+            printf "\nDigite um comando válido.\n"
+            continue
 
-    fi
+        fi
 
-done
+    done
 
 ## 2 - Chama o script que criar novos diretórios, grupos e usuários
 
-while true; do
+    while true; do
 
-    read -n 1 -p "Deseja criar novos diretórios, grupos e usuários? [y/n] " ans_b2
+        read -n 1 -p "Deseja criar novos diretórios, grupos e usuários? [y/n] " ans_b2
 
-    if [ $ans_b2 = "y" ]; then
-        ./modules/create_new_gud.sh
-        break
+        if [ $ans_b2 = "y" ]; then
+            ./modules/create_new_gud.sh
+            break
 
-    elif [ $ans_b2 = "n" ]; then
-        printf "\n...\n"
-        break
+        elif [ $ans_b2 = "n" ]; then
+            printf "\n...\n"
+            break
 
-    else
-        printf "\nDigite um comando válido.\n"
-        continue
+        else
+            printf "\nDigite um comando válido.\n"
+            continue
 
-    fi
+        fi
 
-done
+    done
 
 ## 3 - Fim
-printf "\nFinalizado.\n"
+
+    printf "\n${GREEN}Finalizado.${NC}\n"
